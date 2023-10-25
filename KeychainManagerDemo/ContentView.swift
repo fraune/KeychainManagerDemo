a7a69afe91a669bc246b0d9a4a1eef7c25a62b49
Brandon Fraune <brandon.fraune@gmail.com>
2023-10-25 18:37:22 -0500

    copy the demo over from the other project

--- KeychainManagerDemo/ContentView.swift
+++ KeychainManagerDemo/ContentView.swift
@@ -2,20 +2,107 @@
 //  ContentView.swift
 //  KeychainManagerDemo
 //
-//  Created by Brandon Fraune on 10/25/23.
+//  Created by Brandon Fraune on 7/17/23.
 //
 
 import SwiftUI
+import OktaSecureStorage
+import KeychainAccess
+import Valet
 
 struct ContentView: View {
+    @State var oktaSecureStorageKey = ""
+    @State var oktaSecureStorageValue = ""
+    @State var keychainAccessKey = ""
+    @State var keychainAccessValue = ""
+    @State var valetKey = ""
+    @State var valetValue = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section(header: Text("OktaSecureStorage")) {
+                HStack {
+                    Text("Key")
+                    TextField("Key", text: $oktaSecureStorageKey)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                }
+                HStack {
+                    Text("Value")
+                    TextField("Value", text: $oktaSecureStorageValue)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                }
+                Button("Get") {
+                    let okta = OktaSecureStorage()
+                    do {
+                        oktaSecureStorageValue = try okta.get(key: oktaSecureStorageKey)
+                    } catch {
+                        print("Okta could not get")
+                    }
+                }
+                Button("Set") {
+                    let okta = OktaSecureStorage()
+                    do {
+                        try okta.set(oktaSecureStorageValue, forKey: oktaSecureStorageKey)
+                    } catch {
+                        print("Okta could not set")
+                    }
+                }
+            }
+            Section(header: Text("KeychainAccess")) {
+                HStack {
+                    Text("Key")
+                    TextField("Key", text: $keychainAccessKey)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                }
+                HStack {
+                    Text("Value")
+                    TextField("Value", text: $keychainAccessValue)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                }
+                Button("Get") {
+                    let keychain = Keychain()
+                    keychainAccessValue = keychain[keychainAccessKey] ?? ""
+                }
+                Button("Set") {
+                    let keychain = Keychain()
+                    keychain[keychainAccessKey] = keychainAccessValue
+                }
+            }
+            Section(header: Text("Valet")) {
+                HStack {
+                    Text("Key")
+                    TextField("Key", text: $valetKey)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                }
+                HStack {
+                    Text("Value")
+                    TextField("Value", text: $valetValue)
+                        .autocorrectionDisabled()
+                        .textInputAutocapitalization(.never)
+                }
+                Button("Get") {
+                    let valet = Valet.valet(with: Identifier(nonEmpty: "Test Value")!, accessibility: .afterFirstUnlock)
+                    do {
+                        valetValue = try valet.string(forKey: valetKey)
+                    } catch {
+                        print("Valet could not get")
+                    }
+                }
+                Button("Set") {
+                    let valet = Valet.valet(with: Identifier(nonEmpty: "Test Value")!, accessibility: .afterFirstUnlock)
+                    do {
+                        try valet.setString(valetValue, forKey: valetKey)
+                    } catch {
+                        print("Valet could not set")
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
