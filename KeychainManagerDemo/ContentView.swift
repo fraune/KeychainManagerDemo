fde5543a3a95f74a025cbb7799434486a06f4999
Brandon Fraune <brandon.fraune@gmail.com>
2023-10-25 18:31:51 -0500

    initial commit

---
+++ KeychainManagerDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeychainManagerDemo
+//
+//  Created by Brandon Fraune on 10/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeychainManagerDemo/KeychainManagerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainManagerDemoApp.swift
+//  KeychainManagerDemo
+//
+//  Created by Brandon Fraune on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct KeychainManagerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
